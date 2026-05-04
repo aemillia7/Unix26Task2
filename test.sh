76198cbbc46cc99a26ccf068c8ace8b6ca136712
empo1010 <emilija.podolska@mif.stud.vu.lt>
2026-05-05 00:44:29 +0300

    task: add revised version of test.sh (work in progress)

--- test.sh
+++ test.sh
@@ -1,8 +1,121 @@
 #!/usr/bin/bash
 set -uo pipefail
 
+PASS=0
+FAIL=0
+SKIP=0
+
+check () {
+	test_name="$1"
+	expected="$2"
+	command="$3"
+	eval "$command" >/dev/null 2>&1
+	actual=$?
+
+	if [[ "$actual" -eq "$expected" ]]; then
+		PASS=$((PASS + 1))
+		echo "PASS: $test_name --> expected $expected, got $actual"
+	else
+		FAIL=$((FAIL + 1))
+		echo "FAIL: $test_name --> expected $expected, got $actual"
+	fi
+}
+
 for cmd in /bin/bb-*; do
-	timeout 2 "$cmd" --help </dev/null >/dev/null 2>&1
-	exit_code=$?
-	echo "$cmd --> $exit_code"
+	name="$(basename "$cmd")"
+
+	case "$name" in
+		bb-acpid|bb-arping|bb-killall5|bb-reboot|bb-poweroff|bb-halt|bb-shutdown|bb-init|bb-mount|bb-umount|bb-fdisk|bb-fsck*|bb-mkfs*|bb-adduser|bb-deluser|bb-passwd|bb-ifconfig|bb-ip|bb-modprobe|bb-insmod|bb-rmmod)
+			check "$name --help" 0 "$cmd --help"
+			;;
+		bb-\[)
+			check "$name" 0 'bb-[ -f /etc/passwd ]'
+			;;
+		bb-\[\[)
+			check "$name" 0 'bb-[[ -f /etc/passwd ]]'
+			;;
+		bb-add-shell)
+
+			;;
+		bb-addgroup)
+
+			;;
+		bb-adjtimex)
+
+			;;
+		bb-arch)
+			check "$name" 0 'bb-arch'
+			;;
+		bb-arp)
+			check "$name" 0 'bb-arp'
+			;;
+		bb-ascii)
+
+			;;
+		bb-ash)
+			
+			;;
+		bb-awk)
+
+			;;
+		bb-base32)
+
+			;;
+		bb-base64)
+
+			;;
+		bb-basename)
+
+			;;
+		bb-bc)
+
+			;;
+		bb-beep)
+
+			;;
+		bb-blkdiscard)
+
+			;;
+		bb-blkid)
+
+			;;
+		bb-blockdev)
+
+			;;
+		bb-bootchartd)
+
+			;;
+		bb-brctl)
+
+			;;
+		bb-bunzip2)
+
+			;;
+		bb-bzcat)
+
+			;;
+		bb-bzip2)
+
+			;;
+
+
+
+
+
+
+
+
+
+		*)
+			echo "SKIP: $name"
+			SKIP=$((SKIP + 1))
+			;;
+	esac
 done
+
+
+echo "------------------------"
+echo "PASS: $PASS"
+echo "FAIL: $FAIL"
+echo "SKIP: $SKIP"
+echo "------------------------"
