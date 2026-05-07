705f6b64cc6f30c6e0244b604e78542ff1c65cbd
empo1010 <emilija.podolska@mif.stud.vu.lt>
2026-05-08 02:19:44 +0300

    task: add BusyBox httpd service deployment

--- deploy.sh
+++ deploy.sh
@@ -24,3 +24,38 @@ EOF
 done
 
 echo "BusyBox commands deployed successfully."
+
+
+######################################################################
+
+sudo mkdir -p /var/www/html
+
+# tee parodo terminale outputa ir tuo paciu metu iraso ji i faila, bet naudojamas dev kad nebutu output terminale
+echo "I am alive empo1010" | sudo tee /var/www/html/index.html > /dev/null
+
+
+sudo tee /etc/bb-httpd.conf > /dev/null <<EOF
+EOF
+
+sudo tee /etc/systemd/system/bb-httpd.service > /dev/null <<EOF
+[Unit]
+Description=BusyBox HTTP daemon
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/bin/bb-httpd -f -p 80 -h /var/www/html -c /etc/bb-httpd.conf
+Restart=always
+RestartSec=2
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+# systemd perskaito nauja service faila
+sudo systemctl daemon-reload
+# servisas startuos automatiskai po reboot
+sudo systemctl enable bb-httpd
+# servisas paleidziamas dabar
+sudo systemctl restart bb-httpd
---
+++ httptest.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+set -uo pipefail
+
+bb-wget -qO- http://localhost
